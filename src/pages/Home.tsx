ce988bf0a460e6c50532300c5b6b3712df3c8396
Josilene Silva <josilenevitoriasilva@gmail.com>
2022-06-25 20:01:50 -0400

    feat: home testID

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { 
-    View, 
-    Text, 
-    StyleSheet, 
-    TextInput, 
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
     Platform,
-    FlatList,    
+    FlatList,
 } from 'react-native';
 
 import { Button } from '../components/Button';
@@ -13,25 +13,25 @@ import { SkillCard } from '../components/SkillCard';
 
 interface SkillData {
     id: string;
-    name: string;    
+    name: string;
 }
 
 export function Home(){
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState<SkillData[]>([]);  
-  const [gretting, setGretting] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [greeting, setGreeting] = useState('');
 
-  
-  function handleAddNewSkill(){    
+
+  function handleAddNewSkill(){
     const data = {
-        id: String(new Date().getTime()),        
+        id: String(new Date().getTime()),
         name: newSkill
-    }        
-    
+    }
+
     setMySkills(oldState => [...oldState, data]);
   }
 
-  function handleRemoveSkill(id: string){ 
+  function handleRemoveSkill(id: string){
       setMySkills(oldState => oldState.filter(
           skill => skill.id !== id
       ));
@@ -39,66 +39,66 @@ export function Home(){
 
 
   useEffect(() => {
-      const currentHour = new Date().getHours();      
-      
+      const currentHour = new Date().getHours();
+
       if(currentHour < 12){
-          setGretting('Good morning');
+          setGreeting('Good morning');
       }
       else if(currentHour >= 12 && currentHour < 18){
-          setGretting('Good afternoon');
+          setGreeting('Good afternoon');
       }else{
-          setGretting('Good night');
+          setGreeting('Good night');
       }
-      
+
   }, [])
 
-  return (    
-      <View style={styles.container}>      
-        
+  return (
+      <View style={styles.container}>
 
-        <Text style={styles.title}>
-            Welcome, Rodrigo 
-        </Text>  
+
+        <Text style={styles.title} testID="welcome">
+            Welcome, Rodrigo
+        </Text>
 
         <Text style={styles.greetings}>
-            { gretting }       
-        </Text>   
+            { greeting }
+        </Text>
 
-        <TextInput 
+        <TextInput
             style={styles.input}
             placeholder="New skill"
             placeholderTextColor="#555"
             onChangeText={setNewSkill}
         />
 
-        <Button 
-            title="Add"            
-            onPress={handleAddNewSkill}             
+        <Button
+            title="Add"
+            onPress={handleAddNewSkill}
         />
 
-        <Text style={[styles.title, { marginVertical: 50}]}> 
+        <Text style={[styles.title, { marginVertical: 50}]}>
             MySkills
-        </Text>             
-        
-        <FlatList 
+        </Text>
+
+        <FlatList
             data={mySkills}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
-                <SkillCard 
-                    skill={item.name} 
+                <SkillCard
+                    skill={item.name}
                     onPress={() => handleRemoveSkill(item.id)}
                 />
             )}
         />
-            
+
       </View>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,         
-        backgroundColor: '#121015',        
+        flex: 1,
+        backgroundColor: '#121015',
         paddingVertical: 70,
         paddingHorizontal: 30
     },
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
         padding: Platform.OS === 'ios' ? 15 : 10,
         marginTop: 30,
         borderRadius: 7
-    },    
+    },
     greetings: {
         color: '#FFF'
     }
