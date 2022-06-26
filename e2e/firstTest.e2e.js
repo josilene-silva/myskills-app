931572b0b3d54f3a3ac07eeb2f4b5016f4e9cfc0
Josilene Silva <josilenevitoriasilva@gmail.com>
2022-06-25 20:23:28 -0400

    test: check register a new skill

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,4 +1,4 @@
-describe('Example', () => {
+describe('Home Screen', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -9,5 +9,19 @@ describe('Example', () => {
 
   it('should have welcome screen', async () => {
     await expect(element(by.id('welcome'))).toBeVisible();
+  });
+
+  it('check register a new skill', async () => {
+    const inputNewSkill = await element(by.id('input-new'));
+    const buttonAdd = await element(by.id('button-add'));
+    const flatListSkills = await element(by.id('flat-list-skills'));
+
+    await inputNewSkill.tap();
+    await inputNewSkill.typeText('React Native\n');
+    await buttonAdd.tap();
+
+    await flatListSkills.tap();
+
+    expect(flatListSkills).toBeVisible();
   });
 });
